29612ce282e516e2659b811e05a74ce82a4ff66b
FALEICA <fabio.leite.cardoso@gmail.com>
2022-12-07 00:09:35 -0300

    Project Created

---
+++ src/main/java/com/faleicadev/curso_java_springboot/CursoJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.faleicadev.curso_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faleicadev/curso_java_springboot/CursoJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleicadev.curso_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
