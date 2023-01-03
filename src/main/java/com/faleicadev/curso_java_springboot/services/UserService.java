ba7d924e1a782120981b590bce8edd9763713df1
FALEICA <fabio.leite.cardoso@gmail.com>
2023-01-03 17:20:14 -0300

    DatabaseException update

--- src/main/java/com/faleicadev/curso_java_springboot/services/UserService.java
+++ src/main/java/com/faleicadev/curso_java_springboot/services/UserService.java
@@ -13,6 +13,8 @@ import com.faleicadev.curso_java_springboot.repositories.UserRepository;
 import com.faleicadev.curso_java_springboot.services.exceptions.DatabaseException;
 import com.faleicadev.curso_java_springboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -48,10 +50,15 @@ public class UserService {
 		}
 	}
 	
-	public User update(Long ID, User obj) {
-		User entity = repository.getReferenceById(ID);
+	public User update(Long id, User obj) {
+		
+		try {
+		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
